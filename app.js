c8f7e58ad6f403d1176a4a7a3a1fc7683655af5a
kelvinkuria <kkuria383@yahoo.com>
2024-08-24 14:17:50 +0300

    Completed assignment

---
+++ app.js
@@ -0,0 +1,31 @@
+// function printOok(ook){
+//     console.log(ook)
+// }
+
+// const pritnOok1 = ook => (
+//     console.log(ook))
+
+// printOok("kinywaji")
+// pritnOok1("nganuthia")
+// const printOok =(ook,author) => [ook,author]
+// printOok('nganuthia' , "sayssss")
+
+// function foo(){
+//     return "nyanyez"
+
+// }
+// const foo = function(){
+//     return "ar"
+// }
+
+// const add = function(pa1,pa2){
+//     return pa1 +pa2
+// }
+
+// const twoAdder = x => x+2;
+
+const sum = (pam1,pam2) =>{
+    console.log('adding${pam1}');
+    console.log('adding ${pam2}');
+    return pam1+pam2;
+}
